d2fbb2d6e70204eb5cfb692b3a95fb49b2ebb066
Othamae <vero.gconesa@gmail.com>
2023-12-22 18:11:16 +0100

    feat: :sparkles: Added day12

---
+++ day12/part_1/HotSprings.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs'
+
+const filePath = '../input.txt'
+
+
+function HotSprings(filePath: string) {
+    const content = fs.readFileSync(filePath, 'utf-8')
+    const list = content.trim().split('\n')
+    let totalArr = 0
+    for (const line of list) {
+        const [springs, groupStr] = line.split(' ')
+        const group = groupStr.split(',').map(x => Number(x))
+        totalArr += getArrangements(springs as string, group as number[])
+    }
+
+    return totalArr
+}
+
+
+function getArrangements(springs: string, group: number[]): number {
+    const line = springs + ' ' + group.join(',')
+    const cache: { [key: string]: number } = {}
+    if (cache[line]) return cache[line]
+    if (group.length <= 0) return Number(!springs.includes('#'))
+    const totalGroup = group.reduce((a, b) => a + b)
+    if (springs.length - totalGroup - group.length + 1 < 0) return 0
+    const unknown = !springs.slice(0, group[0]).includes(".")
+    if (springs.length === group[0]) return Number(unknown)
+    return cache[line] ??= (springs[0] != '#' ? getArrangements(trimStart(springs.slice(1)), group) : 0)
+        + (unknown && springs[group[0]] != '#' ? getArrangements(trimStart(springs.slice(group[0] + 1)), group.slice(1)) : 0)
+
+}
+
+function trimStart(str: string) {
+    return str.startsWith(".") ? str.split(/(?<=\.)(?=[^.])/).slice(1).join("") : str
+}
+console.log(HotSprings(filePath))
+
+
